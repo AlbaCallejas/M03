d03499ad0a3a1badfee7e2812d6bc75886f52603
Alba Callejas <albacallejas@localhost.localdomain>
2016-02-11 18:37:02 +0100

    Deberes 04-04-2017

---
+++ Deberes para 04-04-2017/PiedraPapelTijeraII.py
@@ -0,0 +1,67 @@
+#coding:utf-8
+
+import os
+
+salir= False
+num=31
+J1=31
+J2=31
+
+while (salir==False):
+	
+	if (J1%7==0) or (J1%7==1):
+		print num,("J1: Tijera")
+	else:
+		if (J1%7==2) or (J1%7==3) or (J1%7==6):
+			print num,("J1: Piedra")
+		else:
+			if (J1%7==4) or (J1%7==5):
+				print num,("J1: Papel")
+		
+	if (J2%5==0) or (J2%5==1):
+		print num,("J2: Papel")
+	else:
+		if (J2%5==2) or (J2%5==3):
+			print num,("J2: Tijera")
+		else:
+			if (J2%5==4):
+				print num,("J2: Piedra")
+	
+	print
+	
+	if ( (J1%7==2) or (J1%7==3) or (J1%7==6) and (J2%5==0) or (J2%5==1)):
+		print ("J2 WINS!")
+	else:
+		if ( (J1%7==2) or (J1%7==3) or (J1%7==6) and (J2%5==2) or (J2%5==3)):
+			print ("J1 WINS!")
+		else:
+			if ( (J1%7==0) or (J1%7==1) and (J2%5==0) or (J2%5==1)):
+				print ("J1 WINS!")
+			else:
+				if ( (J1%7==0) or (J1%7==1) and (J2%5==4)):
+					print ("J2 WINS!")
+				else:
+					if ( (J1%7==4) or (J1%7==5) and (J2%5==2) or (J2%5==3)):
+						print ("J2 WINS!")
+					else:	
+						if ( (J1%7==4) or (J1%7==5) and (J2%5==4)):
+							print ("J1 WINS!")
+	
+	if ((J1%7==0) or (J1%7==1)) and ((J2%5==2) or (J2%5==3)):
+		print ("Empate!!!")
+	else:
+		if ((J1%7==2) or (J1%7==3) or (J1%7==6)) and ((J2%5==4)):
+			print ("Empate!!!")
+		else:
+			if ((J1%7==4) or (J1%7==5)) and ((J2%5==0) or (J2%5==1)):
+				print ("Empate!!!")
+	print
+	
+	if (num==57):
+		salir = True
+	num=num+1
+	J1=J1+1
+	J2=J2+1
+
+	
+
